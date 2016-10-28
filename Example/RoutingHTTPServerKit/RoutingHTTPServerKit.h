0ef8280271d1e6a3f8b591768e45cd43a96c3a9f
Matias Piipari <matias.piipari@gmail.com>
2016-10-28 16:07:01 +0100

    Adds a framework target with a Carthage based dependency to CocoaHTTPServer.

---
+++ Example/RoutingHTTPServerKit/RoutingHTTPServerKit.h
@@ -0,0 +1,19 @@
+//
+//  RoutingHTTPServerKit.h
+//  RoutingHTTPServerKit
+//
+//  Created by Matias Piipari on 28/10/2016.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RoutingHTTPServerKit.
+FOUNDATION_EXPORT double RoutingHTTPServerKitVersionNumber;
+
+//! Project version string for RoutingHTTPServerKit.
+FOUNDATION_EXPORT const unsigned char RoutingHTTPServerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RoutingHTTPServerKit/PublicHeader.h>
+
+
